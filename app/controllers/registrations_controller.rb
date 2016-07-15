ed7427126322ce00a0c16836846a1fa236da0fc2
colpan <colpanius@gmail.com>
2016-07-15 12:26:45 -0700

    add team updating for social users

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
     if resource.encrypted_password.blank? # || params[:password].blank?
       resource.email = params[:email] if params[:email]
+      resource.team = params[:team] if params[:team]
       if !params[:password].blank? && params[:password] == params[:password_confirmation]
         logger.info "Updating password"
         resource.password = params[:password]
