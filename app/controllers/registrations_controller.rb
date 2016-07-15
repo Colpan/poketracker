64bfc3edd2af9115f583a1577c944b61ce91cace
colpan <colpanius@gmail.com>
2016-07-15 12:48:50 -0700

    Updates

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,21 +1,20 @@
 class RegistrationsController < Devise::RegistrationsController
   def update_resource(resource, params)
-    if resource.encrypted_password.blank? # || params[:password].blank?
-      resource.email = params[:email] if params[:email]
-      if params[:team]
-        resource.team = params[:team]
-        resource.save
-      end
-      if !params[:password].blank? && params[:password] == params[:password_confirmation]
-        logger.info "Updating password"
-        resource.password = params[:password]
-        resource.save
-      end
-      if resource.valid?
-        resource.update_without_password(params)
-      end
-    else
-      resource.update_with_password(params)
+    resource.email = params[:email] if params[:email]
+    if params[:team]
+      resource.team = params[:team]
+      user = User.find_by(email: params[:email])
+      user.team = params[:team]
+      user.save
+      resource.save
+    end
+    if !params[:password].blank? && params[:password] == params[:password_confirmation]
+      logger.info "Updating password"
+      resource.password = params[:password]
+      resource.save
+    end
+    if resource.valid?
+      resource.update_without_password(params)
     end
   end
 end
